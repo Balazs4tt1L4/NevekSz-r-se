2f16615287cb42d2c7812013bd9ec14a6d41b1a9
balazsa <balazsa@kkszki.hu>
2025-11-28 09:04:21 +0100

    Add project files.

---
+++ NevekSzűrése/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace NevekSzűrése
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
